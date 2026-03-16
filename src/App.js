60c2013b14b0f5a75f11e6b44581e756b4cd0484
Park Seungjae <andrewpark0619@gmail.com>
2026-03-16 21:32:01 +0900

    feat: 구분자 추출 기능과 숫자 파싱 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Console } from "@woowacourse/mission-utils";
 import { CALCULATOR_MESSAGE } from "./constants/constants.js";
+import { extractDelimiter, parser } from "./utils/Parser.js";
 
 class App {
   async run() {
     try {
-      const stringInput = await Console.readLineAsync(
+      const input = await Console.readLineAsync(
         CALCULATOR_MESSAGE.START_MESSAGE,
       );
+
+      const { delimiter, expression } = extractDelimiter(input);
+      const numberArray = parser(delimiter, expression);
     } catch (error) {
       throw new Error(error.message);
     }
---
+++ src/utils/Parser.js
@@ -0,0 +1,19 @@
+export function extractDelimiter(input) {
+  if (input.startsWith("//")) {
+    const delimiter = input.split("\\n")[0][2];
+    const expression = input.split("\\n")[1];
+
+    return {
+      delimiter,
+      expression,
+    };
+  }
+  return {
+    delimiter: /[,:]/,
+    expression: input,
+  };
+}
+
+export function parser(delimiter, expression) {
+  return expression.split(delimiter).map(Number);
+}
