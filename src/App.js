15354d179d0a67c6d310c93df46f32f134b723d8
Park Seungjae <andrewpark0619@gmail.com>
2026-03-16 19:00:06 +0900

    feat: 사용자 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
+import { CALCULATOR_MESSAGE } from "./constants/constants.js";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const stringInput = await Console.readLineAsync(
+        CALCULATOR_MESSAGE.START_MESSAGE,
+      );
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
 }
 
 export default App;
